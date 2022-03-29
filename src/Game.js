6ebeaf4235ea8ab92531a0ae946ed5f2d0956765
nonme <xdock1good@gmail.com>
2022-03-29 07:25:35 +0300

    Zoom and scrolling via mouse implemented.

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,8 @@ TerrainJS.Game.prototype = {
       "assets/spritesheet.png",
       "assets/spritesheet.json"
     );
+    this.cameras.main.zoomTo(2, 10);
+    this.cameras.main.pan(100, 100, 10);
   },
 
   create: function () {
@@ -23,6 +25,21 @@ TerrainJS.Game.prototype = {
       return;
     }
     this.level = new TerrainJS.WorldBuilder(this).buildFromJSON(json);
+
+    this.input.on("pointermove", function(pointer) {
+      if (!pointer.isDown) return;
+
+      this.cameras.main.scrollX -= (pointer.x - pointer.prevPosition.x) / this.cameras.main.zoom;
+      this.cameras.main.scrollY -= (pointer.y - pointer.prevPosition.y) / this.cameras.main.zoom;
+    });
+
+    this.input.on("wheel", function(pointer, go, dx, dy, dz) {
+      let zoom = this.cameras.main.zoom - (0.01 * dy);
+      if (zoom < 1) zoom = 1;
+      this.cameras.main.zoomTo(zoom, 10);
+      //this.cameras.main.pan(pointer.x, pointer.y, 10);
+      console.log(this.cameras.main.zoom, dy);
+    });
   },
 
   update: function () {},
